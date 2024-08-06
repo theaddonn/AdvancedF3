6d0cb84be46a66ae39865b13676a53e7235df184
theaddon <theaddonn@gmail.com>
2024-08-06 15:08:57 +0200

    start refactor

---
+++ src/dllmain.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <Windows.h>
+#include <amethyst/runtime/AmethystContext.hpp>
+#include <amethyst/runtime/ModContext.hpp>
+
+#include <iomanip>
+#include <iostream>
+
+#define ModFunction extern "C" __declspec(dllexport)
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    return TRUE;
+}
